8c799d683e6ded760e12c2b86f5eb7c13ca1db6e
vinaytv <vinay.tv@gmail.com>
2023-08-31 20:38:47 +0530

    Delete producer directory

--- producer/src/main/java/com/alphasense/producer/Config.java
+++
@@ -1,9 +0,0 @@
-package com.alphasense.producer;
-
-import com.alphasense.cloudservice.service.CloudQueueService;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Config {
-
-}
