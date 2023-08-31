eeeb0eaabc27b6702e4b76993b6ad7e5c4b71810
vinaytv <vinay.tv@gmail.com>
2023-08-31 20:37:49 +0530

    Add files via upload

--- cloudservice/src/main/java/com/alphasense/cloudservice/configuration/CloudServiceConfig.java
+++ cloudservice/src/main/java/com/alphasense/cloudservice/configuration/CloudServiceConfig.java
@@ -1,5 +1,6 @@
 package com.alphasense.cloudservice.configuration;
 
+import com.alphasense.cloudservice.exceptions.CloudQueueServiceException;
 import com.alphasense.cloudservice.service.CloudQueueService;
 import com.alphasense.cloudservice.service.impl.AwsQueue;
 import com.alphasense.cloudservice.service.impl.AzureQueue;
@@ -8,12 +9,20 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudServiceConfig {
+
+
+    @Value("${aws.access_key:#{null}}")
+    private String awsAccessKey;
+
+    @Value("${aws.secret_key:#{null}}")
+    private String awsSecretKey;
 
     @Bean
     @ConditionalOnProperty(prefix = "cloud", name = "service", havingValue = "AZURE")
@@ -25,7 +34,10 @@ public class CloudServiceConfig {
     @Bean(name = "aws-bean")
     @ConditionalOnProperty(prefix = "cloud", name = "service", havingValue = "AWS")
     public AmazonSQS amazonSQSClient() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials("", "");
+        if(awsAccessKey==null || awsSecretKey==null){
+            throw new CloudQueueServiceException("AWS credentials are required for this profile.");
+        }
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsAccessKey);
         return AmazonSQSClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .withRegion("ap-south-1")
